6a09a0b52ab192dd21064df928e375f0f7fc593f
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-01-16 20:56:40 -0600

    Fixed Carpet hardness & resistance

--- src/main/java/mod/equinox/flamboyant/block/BlockProperties.java
+++ src/main/java/mod/equinox/flamboyant/block/BlockProperties.java
@@ -4,7 +4,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 
-
 public class BlockProperties {
     public static final Block.Properties WOOL = Block.Properties.create(Material.WOOL).hardnessAndResistance(0.8F).sound(SoundType.CLOTH);
     public static final Block.Properties TERRACOTTA = Block.Properties.create(Material.ROCK).hardnessAndResistance(1.25F, 4.2F);
@@ -13,4 +12,5 @@ public class BlockProperties {
     public static final Block.Properties CONCRETEPOWDER = Block.Properties.create(Material.SAND).hardnessAndResistance(0.5F).sound(SoundType.SAND);
     public static final Block.Properties GLASS = Block.Properties.create(Material.GLASS).hardnessAndResistance(0.3F).sound(SoundType.GLASS);
     public static final Block.Properties BED = Block.Properties.create(Material.WOOL).sound(SoundType.WOOD).hardnessAndResistance(0.2F);
+    public static final Block.Properties CARPET = Block.Properties.create(Material.CARPET).hardnessAndResistance(0.1F).sound(SoundType.CLOTH);
 }
