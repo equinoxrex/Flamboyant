0f55b954b3b22aa25726147802d2b5a2677c773d
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-01-30 16:08:52 -0600

    Replicating the changes from 1.14

--- src/main/java/mod/equinox/flamboyant/block/BlockProperties.java
+++ src/main/java/mod/equinox/flamboyant/block/BlockProperties.java
@@ -2,6 +2,7 @@ package mod.equinox.flamboyant.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
 import net.minecraft.block.SoundType;
 
 public class BlockProperties {
@@ -9,7 +10,7 @@ public class BlockProperties {
     public static final Block.Properties TERRACOTTA = Block.Properties.create(Material.ROCK).hardnessAndResistance(1.25F, 4.2F);
     public static final Block.Properties GLAZEDTERRACOTTA = Block.Properties.create(Material.ROCK).hardnessAndResistance(1.4F);
     public static final Block.Properties CONCRETE = Block.Properties.create(Material.ROCK).hardnessAndResistance(1.8F);
-    public static final Block.Properties CONCRETEPOWDER = Block.Properties.create(Material.SAND).hardnessAndResistance(0.5F).sound(SoundType.SAND);
+    public static final Block.Properties CONCRETEPOWDER = Block.Properties.create(Material.SAND).harvestTool(ToolType.SHOVEL).hardnessAndResistance(0.5F).sound(SoundType.SAND);
     public static final Block.Properties GLASS = Block.Properties.create(Material.GLASS).hardnessAndResistance(0.3F).sound(SoundType.GLASS);
     public static final Block.Properties BED = Block.Properties.create(Material.WOOL).sound(SoundType.WOOD).hardnessAndResistance(0.2F);
     public static final Block.Properties CARPET = Block.Properties.create(Material.CARPET).hardnessAndResistance(0.1F).sound(SoundType.CLOTH);
